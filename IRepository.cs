b6a8ca852d518d8abfaf0b319397e2773db7008c
Ricardo Peres <rjperes@hotmail.com>
2013-12-30 15:24:10 +0000

    Initial version

---
+++ IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DevelopmentWithADot.Repository
+{
+	public interface IRepository
+	{
+		T Create<T>();
+
+		T Find<T>(params Object[] ids);
+
+		IQueryable<T> Query<T>();
+		IQueryable<T> QueryAndFetch<T>(params Expression<Func<T, Object>>[] paths);
+
+		void Attach<T>(T item);
+		void Detach<T>(T item);
+
+		void Save<T>(T item);
+		void Update<T>(T item);
+		void Delete<T>(T item);
+	}
+}
---
+++ IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DevelopmentWithADot.Repository
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		Boolean HasChanges { get; }
+
+		void Clear();
+
+		void SaveChanges();
+	}
+}
